ce9081c35bd1e36e24e0164edef9b3d0cc1f483b
Jack Atkerson <jatkerson18@gmail.com>
2022-05-28 16:19:33 -0500

    Basic JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
-var getWeather = function() {
-    console.log("Here's ya weatha kid");
+var citySearchEl = document.querySelector ("#search-city");
+var cityInputEl = document.querySelector("#city");
+var searchHistoryEl = document.querySelector("#search-history")
+
+var getWeather = function(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=b624f41aa968c07ea2542c035a761c0f";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayWeather(data, city);
+            });
+        } else {
+            alert("Error: City Not Found");
+        }
+    })
+    .catch(function(error) {
+        alert("Unable to connect to Weather Man");
+    });
+
+    console.log(apiUrl);
+};
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    var city = cityInputEl.ariaValueMax.trim();
+
+    if(city) {
+        getWeather(city);
+        cityInputEl = "";
+    } else {
+        alert("Please enter a valid city name");
+    }
+};
+
+var displayWeather = function() {
+    console.log("More weather? sheesh");
 };
 
 getWeather();
